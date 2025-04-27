2d29c16967573900d3ccd419c4aab07ae4f86db2
unknown <theoladejitoluwa@gmail.com>
2025-04-27 01:38:16 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        opayGreen: "#28C36E", // Your custom color
+      },
+    },
+  },
+  plugins: [],
+};
